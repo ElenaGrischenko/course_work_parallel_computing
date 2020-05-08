0f16daa21f91823d077d73d8493cfc89d239a173
Olena Hryshchenko <helengrischenko00@gmail.com>
2020-05-08 14:00:57 +0300

    fix: add an inverted index entry to a file

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,51 +7,47 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
-    public static int NUMBER_THREADS = 1;
-    public static void main(String[] args) throws IOException, InterruptedException {
+
+    public static int NUMBER_THREADS = 3;
+
+    public static void main(String[] args) throws IOException {
+
         ArrayList<File> files = new Files().NeededFiles();
-        HashMap<String, File> InvertedIdx = new HashMap<String, File>();
-        int Size = files.size();
+
+        HashMap<String, ArrayList<String>> FullInvertedIndex = new HashMap<String, ArrayList<String>>();
+
         InvertedIndex ThreadArray[] = new InvertedIndex[NUMBER_THREADS];
+
         for(int i = 0; i < NUMBER_THREADS; i++) {
-            ThreadArray[i] = new InvertedIndex(Size/NUMBER_THREADS * i, i == (NUMBER_THREADS - 1)?Size:Size/NUMBER_THREADS * (i-1), files);
+            ThreadArray[i] = new InvertedIndex(files.size()/NUMBER_THREADS * i,
+                    i == (NUMBER_THREADS - 1)?files.size():files.size()/NUMBER_THREADS * (i-1),
+                    files);
             ThreadArray[i].start();
         }
-        for (int i = 0; i < NUMBER_THREADS; i++) {
-            ThreadArray[i].join();
-        }
-        FileWriter fw = new FileWriter( "C:\\InvertedIdx.txt" );
-        for (int i = 0; i < NUMBER_THREADS; i++) {
-            for(Map.Entry<String, String> entry: ThreadArray[i].getInvertedIdx().entrySet())
-                fw.write(entry.getKey() + " - " + entry.getValue() + "\n");
-        }
-        fw.close();
 
-
-        /*
-        File folder = new File("C:\\aclImdb\\test\\neg");
-        ArrayList<File> ff = new ArrayList<>();
-        for ( k = N/50*28; k < N/50*29; k++) {
-            for (File file : folder.listFiles()) {
-                    if (file.getName().startsWith(String.valueOf(k))) {
-                        ff.add(file);
-                    }
+        for (int i = 0; i < NUMBER_THREADS; i++) {
+            try {
+                ThreadArray[i].join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
         }
-        System.out.println(ff.size());
 
-        for(k = 0; k < ff.size(); k++) {
-            FileReader fr = new FileReader(ff.get(k).getAbsolutePath());
-            Scanner scan = new Scanner(fr);
-            //System.out.println(scan.nextLine());
-            fr.close();
+        for (int OneThread = 0; OneThread < NUMBER_THREADS; OneThread++) {
+            ThreadArray[OneThread].getInvertedIdx().forEach((k, v) -> FullInvertedIndex.merge(k, v, (v1, v2) -> {
+                ArrayList<String> list = new ArrayList<>(v1);
+                list.addAll(v2);
+                return list;
+            }));
         }
-        FileReader fr = new FileReader(ff.get(0).getAbsolutePath());
-        Scanner scan = new Scanner(fr);
 
-        //System.out.println(Arrays.toString(scan.nextLine().split("\\s*(\\s|,|!|'|:|;|-|_|\\.)\\s*")));
-        fr.close();
-        */
+        FileWriter fw = new FileWriter( "C:\\InvertedIdx.txt" );
+
+        for(Map.Entry<String, ArrayList<String>> entry: FullInvertedIndex.entrySet()) {
+            fw.write(entry.getKey() + " : " + entry.getValue() + "\n");
+        }
+
+        fw.close();
     }
 }
 
