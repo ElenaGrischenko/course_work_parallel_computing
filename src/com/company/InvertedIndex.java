654c4954fb02a26e49a62833ee0e6b6c7f13a1b1
Olena Hryshchenko <helengrischenko00@gmail.com>
2020-05-10 00:02:02 +0300

    fix: refactor run method

--- src/com/company/InvertedIndex.java
+++ src/com/company/InvertedIndex.java
@@ -7,10 +7,13 @@ import java.io.IOException;
 import java.util.*;
 
 public class InvertedIndex extends Thread {
+
     int startIndex;
     int endIndex;
+
     ArrayList<File> processingFiles;
     HashMap<String, ArrayList<String>> HashMapOneThread = new HashMap<String, ArrayList<String>>();
+
     public InvertedIndex(int startIndex, int endIndex, ArrayList<File> processingFiles) {
         this.startIndex = startIndex;
         this.endIndex = endIndex;
@@ -21,46 +24,46 @@ public class InvertedIndex extends Thread {
         return HashMapOneThread;
     }
 
-    public void addWordToHashMap(String key, int WordPosition) {
-        if(HashMapOneThread.containsKey(key)) {
-            ArrayList<String> list = HashMapOneThread.get(key);
-            list.add(String.valueOf(WordPosition + 1));
-            HashMapOneThread.put(key, list);
-        }
-        else {
-            ArrayList<String> list = new ArrayList<String>();
-            list.add(String.valueOf(WordPosition + 1));
-            HashMapOneThread.put(key, list);
-        }
-    }
-
     @Override
     public void run() {
+
         for (int idx = startIndex; idx < endIndex; idx++) {
             try (BufferedReader file = new BufferedReader(new FileReader((File) processingFiles.get(idx)))) {
                 String line;
                 String[] words = new String[0];
+
                 while ((line = file.readLine()) != null) {
                     words = line.replaceAll("[^a-zA-Z0-9 ]", "")
-                            .replaceAll(" +", " ")
                             .trim()
+                            .replaceAll(" +", " ")
                             .toLowerCase()
                             .split(" ");
                 }
-                int PositionInLine = 0;
-                for (String word: words) {
-                     addWordToHashMap(word, PositionInLine);
-                     PositionInLine++;
+
+                for (int index = 0; index < words.length; index++) {
+                    if(HashMapOneThread.containsKey(words[index])) {
+                        ArrayList<String> list = HashMapOneThread.get(words[index]);
+                        list.add(String.valueOf(index + 1));
+                        HashMapOneThread.put(words[index], list);
+                    }
+                    else {
+                        ArrayList<String> list = new ArrayList<String>();
+                        list.add(String.valueOf(index + 1));
+                        HashMapOneThread.put(words[index], list);
+                    }
                 }
+
                 for (String word: words) {
                     ArrayList<String> list = HashMapOneThread.get(word);
+
                     if (!list.contains(processingFiles.get(idx).getParent() + "\\" + processingFiles.get(idx).getName())) {
                         list.add(processingFiles.get(idx).getParent() + "\\" + processingFiles.get(idx).getName());
                         HashMapOneThread.put(word, list);
                     }
                 }
+
             }catch (IOException e) {
-                System.out.println("File:  not found.");
+                System.out.println("File: " + processingFiles.get(idx).getParent() + "\\" + processingFiles.get(idx).getName() + " not found.");
             }
         }
     }
